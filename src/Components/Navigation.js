41dcc9fa5e15783fa1c6282540944bd97ad0c886
Mohit <mohitgadhavi1@gmail.com>
2022-03-25 09:05:48 +0530

    reg

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import avatar from "../img/avatar.jpg";
 import LaunchIcon from "@mui/icons-material/Launch";
 
@@ -18,26 +18,31 @@ function Navigation() {
         </li>
         <li className="nav-item">
           <NavLink to="/work" activeClassName="active-class" exact>
-            Work
+            Portfolio
           </NavLink>
         </li>
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <NavLink to="/resume" activeClassName="active-class" exact>
             Resume
           </NavLink>
-        </li>
+        </li> */}
 
         <li className="nav-item">
-          <NavLink to="/blogs" activeClassName="active-class" exact>
+          <a
+            href="https://medium.com/@mohitgadhavi1"
+            target="_blank"
+            activeClassName="active-class"
+            exact
+          >
             Blogs <LaunchIcon className="nav-item-icon" />{" "}
-          </NavLink>
+          </a>
         </li>
 
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <NavLink to="/contact" activeClassName="active-class" exact>
             Contact
           </NavLink>
-        </li>
+        </li> */}
       </ul>
     </NavigationStyled>
   );
@@ -49,7 +54,8 @@ const NavigationStyled = styled.nav`
   flex-direction: column;
   align-items: center;
   width: 100%;
-
+  align-items: center;
+  justify-content: center;
   border-right: 1px solid var(--border-color);
   .avatar {
     width: 100%;
@@ -65,21 +71,23 @@ const NavigationStyled = styled.nav`
 
   .nav-items {
     width: 100%;
+
     .nav-item-icon {
       position: absolute;
       margin-left: 1rem;
       padding-bottom: 5px;
     }
-    text-align: center;
+
     .active-class {
       background-color: var(--primary-color-light);
       color: white;
     }
     li {
       display: block;
+
       a {
         display: block;
-        padding: 0.35rem 0;
+        padding: 0.7rem 5rem;
         position: relative;
         z-index: 10;
         text-transform: uppercase;
